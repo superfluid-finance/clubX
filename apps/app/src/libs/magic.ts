bb10e8a922d745962fe9a21059a3df1cf105139a
Mehul Soni <mehul.soni.estonia@gmail.com>
2023-09-05 12:42:52 +0300

    magic wallet

--- apps/app/src/libs/magic.ts
+++ apps/app/src/libs/magic.ts
@@ -1,10 +1,3 @@
-import { Networks } from '@/utils/networks';
-import { EthNetworkConfiguration, Magic } from 'magic-sdk';
 
 
-export const magic = new Magic(process.env.REACT_APP_MAGIC_API_KEY as string, {
-  network: {
-    rpcUrl: process.env.REACT_APP_POLYGON_RPC_URL as string,
-    chainId: 80001,
-  },
-});
+
