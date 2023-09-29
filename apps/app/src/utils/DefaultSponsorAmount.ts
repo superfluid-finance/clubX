1e21acfd3ef2e9ebac8f332a1ca3d3ca13756ab5
Mikk <mikk@superfluid.finance>
2023-09-29 14:49:09 +0300

    Improved sponsor amount logic

--- apps/app/src/utils/DefaultSponsorAmount.ts
+++ apps/app/src/utils/DefaultSponsorAmount.ts
@@ -1,16 +1,13 @@
 const getDefaultSponsorAmount = (sponsorLvl: number | undefined = 1) => {
-  if (sponsorLvl == 1) {
-    return 100000000000000000n;
-  } else if (sponsorLvl == 2) {
-    return 50000000000000000n;
-  } else if (sponsorLvl == 3) {
-    return 20000000000000000n;
-  } else if (sponsorLvl == 4) {
-    return 10000000000000000n;
-  } else if (sponsorLvl == 5) {
-    return 5000000000000000n;
-  } else {
-    return 1000000000000000n;
+  switch (sponsorLvl) {
+    case 1:
+      return 1000000000000000000n;
+    case 2:
+      return 460000000000000000n;
+    case 3:
+      return 190000000000000000n;
+    default:
+      return 0n;
   }
 };
 
