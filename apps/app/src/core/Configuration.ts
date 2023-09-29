d8dd16d16f320893efc93837772109be36d38ee7
Mikk <mikk@superfluid.finance>
2023-09-29 11:38:40 +0300

    Testing the rizz club

--- apps/app/src/core/Configuration.ts
+++ apps/app/src/core/Configuration.ts
@@ -1,10 +1,10 @@
 import { Address } from "viem";
-import { polygonMumbai } from "viem/chains";
+import { polygon } from "viem/chains";
 
 const Configuration = {
-  network: polygonMumbai,
+  network: polygon,
   CFAv1ForwarderAddress: <Address>"0xcfA132E353cB4E398080B9700609bb008eceB125",
-  SuperfluidClubAddress: <Address>"0x5e3fea4a014e84679e839ff0e9b1fbcb7b5096e0",
+  SuperfluidClubAddress: <Address>"0x58b5bcfddf72579fc1e03874df9bc234bc9f417b", //0x5e3fea4a014e84679e839ff0e9b1fbcb7b5096e0
   WalletConnectID: "8f889a96cfb4b5e9cc538b3dae8275d8",
   MagicLinkKey: "pk_live_1AC13096CD7EA885",
 };
