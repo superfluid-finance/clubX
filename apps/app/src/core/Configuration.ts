8de1a555469276231cca86da0f171f6958f3e046
Mikk <mikk@superfluid.finance>
2023-10-04 11:35:39 +0300

    New club address

--- apps/app/src/core/Configuration.ts
+++ apps/app/src/core/Configuration.ts
@@ -4,7 +4,7 @@ import { polygon } from "viem/chains";
 const Configuration = {
   network: polygon,
   CFAv1ForwarderAddress: <Address>"0xcfA132E353cB4E398080B9700609bb008eceB125",
-  SuperfluidClubAddress: <Address>"0x58b5bcfddf72579fc1e03874df9bc234bc9f417b", //0x5e3fea4a014e84679e839ff0e9b1fbcb7b5096e0
+  SuperfluidClubAddress: <Address>"0xf187fa4B5904bC3F6E0E409d80D5213A2e0FF8B2",
   WalletConnectID: "8f889a96cfb4b5e9cc538b3dae8275d8",
   MagicLinkKey: "pk_live_1AC13096CD7EA885",
 };
