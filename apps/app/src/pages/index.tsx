b39a471c4fb9e152f954048ce921ffa901e34dc4
Mikk <mikk@superfluid.finance>
2023-10-12 15:59:37 +0300

    Fixed SSR bug

--- apps/app/src/pages/index.tsx
+++ apps/app/src/pages/index.tsx
@@ -2,18 +2,20 @@ import HomeView from "@/components/HomeView";
 import IntroScreens from "@/components/IntroScreens";
 import LockScreen from "@/components/LockScreen";
 import { useIsProtege } from "@/core/Api";
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
 const Intro = () => {
   const { address } = useAccount();
   const { data: isProtege } = useIsProtege(address);
 
-  if (isProtege === true) {
-    return <HomeView />;
-  }
+  const [isMounted, setIsMounted] = useState(false);
 
-  if (isProtege === false) {
-    return <LockScreen />;
+  useEffect(() => setIsMounted(true), []);
+
+  if (isMounted) {
+    if (isProtege === true) return <HomeView />;
+    if (isProtege === false) return <LockScreen />;
   }
 
   return <IntroScreens />;
